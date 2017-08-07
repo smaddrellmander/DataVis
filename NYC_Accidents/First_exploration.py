68987ba94fc7ca4f97781c6a6cc5125dca5c4519
Samuel Maddrell-Mander <samuel.maddrell-mander@cern.ch>
2017-08-07 11:27:57 +0100

    First exploration file

---
+++ NYC_Accidents/First_exploration.py
@@ -0,0 +1,6 @@
+# First exploation of NYC Taxi acciendt data
+import pandas as pd
+import matplotlib.pyplot as plt
+
+__author__ = "Sam Maddrell-Mander"
+__copyright__ = "University of Bristol, August 2017"
